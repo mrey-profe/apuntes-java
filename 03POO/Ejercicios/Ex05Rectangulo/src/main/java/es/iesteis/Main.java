33cea4f0d88dc74fa6f71289f8c7856ea35da7f0
Marta Rey López <marta.rey@edu.xunta.gal>
2025-03-19 10:31:36 +0100

    Ex05 Rectángulo

--- 03POO/Ejercicios/Ex05Rectangulo/src/main/java/es/iesteis/Main.java
+++ 03POO/Ejercicios/Ex05Rectangulo/src/main/java/es/iesteis/Main.java
@@ -2,6 +2,10 @@ package es.iesteis;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Rectangulo r = new Rectangulo(3, 4);
+        r.amosarInformacion();
+        r.setBase(45);
+        r.amosarInformacion();
+        System.out.println("Altura: "+r.getAltura());
     }
 }
