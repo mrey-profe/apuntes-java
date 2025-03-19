4e387b2db6e78da66d61d10cf24ed5e17b3fc37c
Marta Rey López <marta.rey@edu.xunta.gal>
2025-03-19 10:37:36 +0100

    Ex06Libro

---
+++ 03POO/Ejercicios/Ex06Libros/src/main/java/es/iesteis/Main.java
@@ -0,0 +1,8 @@
+package es.iesteis;
+
+public class Main {
+    public static void main(String[] args) {
+        Libro l = new Libro("1234567890", "El Quijote", "Cervantes", 200);
+        System.out.println(l);
+    }
+}
