396236f43c5187a5bf0a6ac69bb7a493df5e48c2
Marta Rey López <marta.rey@edu.xunta.gal>
2025-03-19 12:39:54 +0100

    empezaPor en cadeas

--- 04Estructuras_de_datos/Exercicios/Ex01Cadeas/src/main/java/es/iesteis/Main.java
+++ 04Estructuras_de_datos/Exercicios/Ex01Cadeas/src/main/java/es/iesteis/Main.java
@@ -3,5 +3,6 @@ package es.iesteis;
 public class Main {
     public static void main(String[] args) {
         OperacionsCadeas.amosarCaracteres("Ola que tal?");
+        System.out.println(OperacionsCadeas.empezaPor("Hola", "Hasadsdfd"));
     }
 }
--- 04Estructuras_de_datos/Exercicios/Ex01Cadeas/src/main/java/es/iesteis/OperacionsCadeas.java
+++ 04Estructuras_de_datos/Exercicios/Ex01Cadeas/src/main/java/es/iesteis/OperacionsCadeas.java
@@ -6,4 +6,17 @@ public class OperacionsCadeas {
             System.out.println(cadea.charAt(i));
         }
     }
+
+    public static boolean empezaPor(String cadea, String subcadea) {
+        if (cadea.length() < subcadea.length()) {
+            System.out.println("A subcadea debe ter menor lonxitude");
+        }
+        boolean inicioIgual = true;
+        for(int i = 0; i < subcadea.length() && inicioIgual; i++) {
+            if (cadea.charAt(i) != subcadea.charAt(i)) {
+                inicioIgual = false;
+            }
+        }
+        return inicioIgual;
+    }
 }
