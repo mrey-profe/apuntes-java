77dd182ba6b37b40c4664a18f5aedccd887f6613
Marta Rey López <marta.rey@edu.xunta.gal>
2025-03-24 15:13:57 +0100

    Exercicios cadeas 1 varios apartados

--- 04Estructuras_de_datos/SolucionsExercicios/Ex01Cadeas/src/main/java/es/iesteis/Main.java
+++ 04Estructuras_de_datos/SolucionsExercicios/Ex01Cadeas/src/main/java/es/iesteis/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         OperacionsCadeas.amosarCaracteres("Ola que tal?");
         System.out.println(OperacionsCadeas.empezaPor("Hola", "Hasadsdfd"));
+        System.out.println(OperacionsCadeas.contarCaracteres("Blablabla", 'a'));
+        System.out.println(OperacionsCadeas.contarPalabras("  a  Hola que   tal  fffa"));
+        System.out.println(OperacionsCadeas.iniciaisMaiusculas("  a  Hola que   tal  fffa"));
+        System.out.println(OperacionsCadeas.inverter("Blablabla"));
+        System.out.println(OperacionsCadeas.substituirCaracteres("Blablabla", 'a', 'o'));
+        System.out.println(OperacionsCadeas.contenSubcadea("La blanca casa", "cal"));
     }
 }
--- 04Estructuras_de_datos/SolucionsExercicios/Ex01Cadeas/src/main/java/es/iesteis/OperacionsCadeas.java
+++ 04Estructuras_de_datos/SolucionsExercicios/Ex01Cadeas/src/main/java/es/iesteis/OperacionsCadeas.java
@@ -19,4 +19,80 @@ public class OperacionsCadeas {
         }
         return inicioIgual;
     }
+
+    public static int contarCaracteres(String cadea, char caracter) {
+        int contador = 0;
+        for (int i = 0; i < cadea.length(); i++) {
+            if (cadea.charAt(i) == caracter) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int contarPalabras(String cadea) {
+        int contador = 0;
+        for (int i = 1; i < cadea.length(); i++) {
+            if (cadea.charAt(i) == ' ' && cadea.charAt(i - 1) != ' ') {
+                contador ++;
+            }
+        }
+        //Falta contar a última palabra
+        if (cadea.charAt(cadea.length() - 1) != ' ') {
+            contador++;
+        }
+        return contador;
+    }
+
+    public static String iniciaisMaiusculas(String cadea) {
+        String resultado = String.valueOf(Character.toUpperCase(cadea.charAt(0)));
+        // Meto o primeiro carácter en maiúsculas
+        for (int i = 1; i < cadea.length(); i++) {
+            if (cadea.charAt(i) != ' ' && cadea.charAt(i-1) == ' ') {
+                resultado += Character.toUpperCase(cadea.charAt(i));
+            } else {
+                resultado += cadea.charAt(i);
+            }
+        }
+        return resultado;
+    }
+
+    public static String inverter(String cadea) {
+        String resultado = "";
+        for (int i = cadea.length() - 1; i >= 0; i--) {
+            resultado += cadea.charAt(i);
+        }
+        return resultado;
+    }
+
+    public static String substituirCaracteres(String cadea, char orixe, char destino) {
+        String resultado = "";
+        for (int i = 0; i < cadea.length(); i++) {
+            if (cadea.charAt(i) == orixe) {
+                resultado += destino;
+            } else {
+                resultado += cadea.charAt(i);
+            }
+        }
+        return resultado;
+    }
+
+    public static boolean contenSubcadea(String cadea, String subcadea) {
+        for (int i = 0; i < cadea.length(); i++) {
+            boolean coincide = true;
+            for (int j = 0; j < subcadea.length(); j++) {
+                if ((i+j) >= cadea.length() || cadea.charAt(i + j) != subcadea.charAt(j)) {
+                    coincide = false;
+                    break;
+                }
+            }
+            if (coincide) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    
 }
